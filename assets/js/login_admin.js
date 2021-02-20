2f2470bf1031f34fd666c12b4999664ea7a5c603
aldayan <67208347+aldayan@users.noreply.github.com>
2021-02-19 22:59:53 -0400

    git remote add origin https://github.com/aldayan/Proyecto-web-de-prog-3-grupo-4.git

---
+++ assets/js/login_admin.js
@@ -0,0 +1,19 @@
+window.addEventListener("resize", anchopagina);
+
+
+function anchopagina() {
+    if (window.innerWidth > 850) {
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+
+    } else {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+    }
+}
+
+anchopagina()
